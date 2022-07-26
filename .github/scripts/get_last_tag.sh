2488966c1a89781e04af55df94376f7ff6c09b3f
shay_levin <shay.levin@perimeter81.com>
2022-07-26 14:23:11 +0200

    update workflow

--- .github/scripts/get_last_tag.sh
+++ .github/scripts/get_last_tag.sh
@@ -2,7 +2,7 @@
 
 
 echo $SEMVER_LEVEL
-
+curl -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USER}'", "password": "'${DOCKERHUB_PASSWD}'"}' https://hub.docker.com/v2/users/login/
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USER}'", "password": "'${DOCKERHUB_PASSWD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
 echo $TOKEN
 export TOKEN=$TOKEN
