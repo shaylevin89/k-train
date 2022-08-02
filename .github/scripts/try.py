f777ad6ed1d4706a037718ab21f6c358ec2acbdb
shay_levin <shay.levin@perimeter81.com>
2022-08-02 14:23:16 +0300

    update workflow

--- .github/scripts/try.py
+++ .github/scripts/try.py
@@ -1,9 +1,19 @@
 import requests
 import os
 
-TOKEN = os.getenv('TOKEN')
 DOCKER_REPO_NAME = os.getenv('DOCKER_REPO_NAME')
 DOCKER_IMAGE_NAME = os.getenv('DOCKER_IMAGE_NAME')
+DOCKERHUB_USER = os.getenv('DOCKERHUB_USER')
+DOCKERHUB_PASSWD = os.getenv('DOCKERHUB_PASSWD')
+
+data = {"username": DOCKERHUB_USER, "password": DOCKERHUB_PASSWD}
+
+
+print(f"{DOCKERHUB_USER} {DOCKERHUB_PASSWD}")
+url = "https://hub.docker.com/v2/users/login/"
+response2 = requests.post(url, data=data)
+print(response2.json()['token'])
+TOKEN = response2.json()['token']
 headers = {
     'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/json',
